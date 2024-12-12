0e4e1711e1add149fb4fe3180adb6461258087cc
허재 <jea1257@naver.com>
2024-12-12 10:17:19 +0900

    feat : 자동차 이름 입력,최대이름 초과시 에러

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -18,7 +18,44 @@ public class RacingGame {
 
     public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
 
+    //자동차 이름 입력 받기
+    List<String> carNames = getCarNames(scanner);
+
+
+    //시행 횟수 입력 받기
+    int numberOfRaces = getNumberOfRaces(scanner);
+
+    //레이스 진행
+        conductRaces(carNames,numberOfRaces);
+
+    }
+    private static List<String> getCarNames(Scanner scanner) {
+
+        //사용자에게 자동차 입력을 받으면 " , " 로 구분
+        System.out.println("\n , ");
+
+        String input = scanner.nextLine();
+
+        //자동차 이름 리스트 반환
+        List<String> carNames = Arrays.asList(input,",");
+
+        //각 자동차 이름 10자인지 검증해야됨
+        for (String name : carNames) {
+            if (name.length()>MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException(" \n[ERROR] 자동차 이름은 10자를 초과할 수 없습니다. ");
+            }
+        }
+        return carNames;
+
+    }
+
+
+
+
+
+    }
 
     }
 
