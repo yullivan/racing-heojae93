55b4681c766def8c046b3aeec6ec39632cc62998
허재 <jea1257@naver.com>
2024-12-12 09:50:28 +0900

    feat : 자동차이름최대길이, 주사위 값, 자동차 전진기준 설정

---
+++ src/main/java/racing/RacingGame.java
@@ -0,0 +1,26 @@
+package racing;
+
+import java.util.*;
+
+public class RacingGame {
+
+    //자동차 이름 최대 길이 10자
+    private static final int MAX_NAME_LENGTH = 10;
+
+    //주사위 최대 값은 6
+    private static final int MAX_DICE_VALUE = 6;
+
+    //주사위 최소 값은 1
+    private static final int MIN_DICE_VALUE = 1;
+
+    //자동차 전진 기준
+    private static final int FORWARD_THRESHOLD = 3;
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
